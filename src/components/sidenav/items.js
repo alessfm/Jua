e25c0b835ed129c7ff93ab0f2f50bf96b8fb0aba
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-08-30 11:13:29 -0300

    png files now working

--- src/components/sidenav/items.js
+++ src/components/sidenav/items.js
@@ -4,6 +4,9 @@ export default function Items()
 {
   return(
     <div>
+    <a className="items">
+      <img className="logo" src="./assets/icons/logo/logo-testmode.png"></img>
+    </a>
     <a className="items">1- Item 1</a>
     <a className="items">2- Item 2</a>
     <a className="items">3- Item 3</a>
--- src/components/sidenav/sidebar.js
+++ src/components/sidenav/sidebar.js
@@ -6,7 +6,6 @@ export default function Sidebar()
 {
   return(
     <div className="sidenav">
-      <Buscador/>
       <Items/>
     </div>
   )
