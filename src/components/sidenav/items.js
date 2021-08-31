251cfddbc54d3f62047eaec96a363a643e82bcd8
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-08-31 14:46:00 -0300

    svg files now custom
    not yet responsive

--- src/components/sidenav/items.js
+++ src/components/sidenav/items.js
@@ -6,11 +6,11 @@ export default function Items()
   return(
     <div>
     <img className="logo" src="./assets/icons/logo/logo-testmode.png"></img>
-    <a className="items"><Search/>busca</a>
-    <a className="items">Item 2</a>
-    <a className="items">Item 3</a>
-    <a className="items">Item 4</a>
-    <a className="items">Item 5</a>
+    <a className="items"><Search/></a>
+    <a className="items"><Search/></a>
+    <a className="items"><Search/></a>
+    <a className="items"><Search/></a>
+    <a className="items"><Search/></a>
     </div>
   )
 }
